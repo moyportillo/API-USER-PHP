4bb69f59162e1a308dd0d9930fb18ccba38095fa
Moises Portillo <moyporti15@gmail.com>
2020-12-10 23:53:22 -0600

    actualizacion v1.0

---
+++ BackEnd/Clases/class-usuario.php
@@ -0,0 +1,93 @@
+<?php 
+    
+    class Usuario{
+        private $nombre;
+        private $apellido;
+        private $identidad;
+        private $tiempo;
+        private $estado;
+
+        public function __construct($nombre, $apellido, $identidad, $tiempo, $estado){
+            $this->nombre = $nombre;
+            $this->apellido = $apellido;
+            $this->identidad = $identidad;
+            $this->tiempo = $tiempo;
+            $this->estado = $estado; 
+        }
+
+        public function asignarNombre($nombre){
+            $this->nombre = $nombre;
+        }
+
+        public function asignarapellido($apellido){
+            $this->apellido = $apellido;
+        }
+
+        public function obtenerNombre(){
+            return $this->nombre;
+        }
+
+        public function obtenerApellido(){
+            return $this->apellido;
+        }
+
+        public function getIdentidad($identidad){
+            $this->getIdentidad = $identidad;
+        }
+
+        public function setIdentidad(){
+            return $this->identidad;
+        }
+
+        public function getTiempo($tiempo){
+            $this->tiempo = $tiempo;
+        }
+
+        public function setTiempo(){
+            return $this->tiempo;
+        }
+
+        public function getEstado($estado){
+            $this->estado = $estado;
+        }
+
+        public function setEstado(){
+            return $this->estado;
+        }
+
+        //Aqui hacer la obtencion del de las operacion
+
+        public function guardarUsuario(){
+            $servername = "127.0.0.1";
+            $username = "root";
+            $password = "01001101Moy*r2-d2";
+            $dbname = "banco";
+            
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            
+            // Check connection
+            if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            }
+            $sql = "INSERT INTO `clientes`(`nombre_user`,`apellido_user`,`id_user`,`user_time_add`,`user_status`)VALUES(?,?,?,?,?);";
+            $statement = $conn->prepare($sql);
+            $statement->bind_param("sssss", $nombre, $apellido, $identidad, $tiempo, $estado);
+            $statement->execute();
+            if($statement){
+                  echo "Se Guardo";
+            }else{
+                echo "No se guardo";
+            }
+        }
+
+        public function obtenerUsuario(){
+
+        }
+
+        public function eliminarUsuario(){
+
+        }
+            
+    }
+?>
---
+++ BackEnd/api/usuarios.php
@@ -0,0 +1,40 @@
+<?php
+
+//echo "Metodo HTTP: ".$_SERVER["REQUEST_METHOD"];
+
+//echo 'Informacion'. file_get_contents('php://input'); //obtener la informacion
+
+include_once ("../Clases/class-usuario.php");
+
+header("Content-Type: application/json");
+switch($_SERVER['REQUEST_METHOD']){
+    case 'POST':
+        $_POST = json_decode(file_get_contents('php://input'), true);
+        $usuario = new Usuario($_POST["nombre"], $_POST["apellido"], $_POST["identidad"], $_POST["tiempo"], $_POST["estado"]);
+        $usuario->guardarUsuario();
+        $resultado["mensaje"] = "Guardar usuario, informacion: ". json_encode($_POST);
+        echo json_encode($resultado);
+        
+    break;
+    case 'GET':
+        if(isset($_GET['id'])){
+            
+            $resultado["mensaje"] = "Retornar el usuario con el id: ".$_GET['id'];
+            echo json_encode($resultado);
+        }else{
+            $resultado["mensaje"] = "Retornar todos los usuarios";
+            echo json_encode($resultado);
+        }
+    break;
+    case 'PUT':
+        $_PUT = json_decode(file_get_contents('php://input'), true);
+        $resultado["mensaje"] = "Actualizar usuario con el id: ". $_GET['id']."Informacion a actualizar: ". json_encode($_PUT);
+        echo json_encode($resultado);
+    break;
+    case 'DELETE':
+        $resultado["mensaje"] = "Eliminar un usuario con el id: ". $_GET['id'];
+        echo json_encode($resultado);
+    break;
+}
+
+?>
